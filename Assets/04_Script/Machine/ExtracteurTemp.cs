f0d9abd6433e6b7f41b00ddd3e9ce4a0604ecea6
ReyonX <rayonxvp4@gmail.com>
2026-07-15 00:34:42 +0200

    avancé machine

---
+++ Assets/04_Script/Machine/ExtracteurTemp.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ExtracteurTemp : MonoBehaviour
+{
+    [Header("Input Items Stocked")]
+    public ItemStock inputStock;
+
+    [Header("Linked Output Pipes")]
+    public TuyauTemp outputPipe;
+
+    private int pipeSpeed;
+
+    private void Start()
+    {
+        InvokeRepeating("RessourceCollector", 1f, 1f);
+        InvokeRepeating("PipeSender", 1f, 1f);
+
+    }
+
+    void RessourceCollector()
+    {
+        inputStock.amountInStock += 1;
+    }
+
+    void PipeSender()
+    {
+        if (outputPipe)
+        {
+            if (inputStock.name == outputPipe.itemInStock.name || outputPipe.itemInStock.name == "")
+            {
+                if (outputPipe.itemInStock.name == "") outputPipe.itemInStock.name = inputStock.name;
+                inputStock.amountInStock -= pipeSpeed;
+                outputPipe.itemInStock.amountInStock += pipeSpeed;
+            }
+        }
+    }
+}
